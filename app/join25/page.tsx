7002e7883f0c37e0f32c83ca4064df86a5df2de0
Jun Yu <awa@outlook.ie>
2025-08-30 14:37:36 -0700

    feat: add 25-26 interest form link

---
+++ app/join25/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function Join25Page() {
+    redirect('https://docs.google.com/forms/d/e/1FAIpQLSeDwUSQIg2a97NV1X8sgn7OiFXGrlpfa8ARMgFlttCn5pFObw/viewform')
+}
