be73e0cbdfd02dfda1c94707c5dcb4a84e016e4f
Jun Yu <awa@outlook.ie>
2025-09-16 16:27:59 -0700

    feat: add shorturl

---
+++ app/signin26/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function Join25Page() {
+    redirect('https://docs.google.com/forms/d/e/1FAIpQLSfHJdfjzsDlUjAR81whK885AHRdo1ViCYNChNBCdpKoGSUVgA/viewform?usp=sharing&ouid=109424922547981011055')
+}
