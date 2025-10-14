958929dc914a63df4c7469db550489d87c8a1032
Jun Yu <awa@outlook.ie>
2025-10-14 14:07:54 -0700

    feat: update

---
+++ app/25oh/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function Join25Page() {
+    redirect('https://docs.google.com/forms/d/e/1FAIpQLSe0Nc2GpOjT1Md7Vs1ipY11zQYiEKqyjPEqHL9MRyYpv1JaJg/viewform')
+}
