8c8a8fdb879b955996dadeb40601783ab9b60c81
Jun Yu <awa@outlook.ie>
2026-01-21 14:03:12 -0800

    feat: add robots

--- app/components/BotIntroCard.tsx
+++ app/components/BotIntroCard.tsx
@@ -10,20 +10,111 @@ import Link from "next/link";
 export default function BotIntroCard() {
     return (
         <div className="max-w-screen-xl sm:px-6 mt-16 mb-18 md:mb-18 mx-auto ui-content">
-        <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 opacity-80 text-orange-600 dark:text-orange-400">&apos;26 REBUILT</h1>
-        <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 mb-8 ">Stay tuned for our new robot.</h1>
-        <p className="text-xl px-6 sm:px-0 mb-8">
-            Our team is excited to announce that we are currently in the process of designing and building our new robot for the 2026 FIRST® Robotics Competition season, REBUILT presented by Haas. 
-            Stay tuned for updates on our journey to create a competitive and inspiring robot for the upcoming season!
-        </p>
-        <br/>
-        <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 opacity-80 text-blue-600 dark:text-blue-400">&apos;25 REEFSCAPE</h1>
-        <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 mb-8 ">Taketsune.</h1>
-        <p className="text-xl px-6 sm:px-0 mb-8">
-          Learn more about our robot for the 2025 season <Link className="a-link" href="/robot/reefscape">REEFSCAPE<sup>SM</sup> presented by Haas: Taketsune</Link>.
-        </p>
-        
-        {/* <CardNavigator />
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 opacity-80 text-orange-600 dark:text-orange-400">&apos;26 REBUILT</h1>
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 mb-8 ">Stay tuned for our new robot.</h1>
+            <p className="text-xl px-6 sm:px-0 mb-8">
+                Our team is excited to announce that we are currently in the process of designing and building our new robot for the 2026 FIRST® Robotics Competition season, REBUILT presented by Haas.
+                We will participate in CA District San Gabriel Valley Event and CA District Orange County Event.
+                Stay tuned for updates on our journey to create a competitive and inspiring robot for the upcoming season!
+            </p>
+            <br />
+
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 opacity-80 text-blue-600 dark:text-blue-400">
+                &apos;25 REEFSCAPE
+            </h1>
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 mb-8 ">
+                Taketsune.
+            </h1>
+            <p className="text-xl px-6 sm:px-0 mb-8">
+                Learn more about our robot for the 2025 season{" "}
+                <Link className="a-link" href="/robot/reefscape">
+                    REEFSCAPE<sup>SM</sup> presented by Haas: Taketsune
+                </Link>
+                .
+            </p>
+
+            <br />
+
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 opacity-80 text-green-600 dark:text-green-400">
+                &apos;24 CRESCENDO
+            </h1>
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 mb-8 ">
+                Takafusa &apos;24.
+            </h1>
+            <p className="text-xl px-6 sm:px-0 mb-8">
+                View our 2024 season on{" "}
+                <Link className="a-link" href="https://www.thebluealliance.com/team/7447/2024">
+                    The Blue Alliance: CRESCENDO
+                </Link>
+                .
+            </p>
+
+            <br />
+
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 opacity-80 text-amber-600 dark:text-amber-400">
+                &apos;23 CHARGED UP
+            </h1>
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 mb-8 ">
+                Takafusa.
+            </h1>
+            <p className="text-xl px-6 sm:px-0 mb-8">
+                View our 2023 season on{" "}
+                <Link className="a-link" href="https://www.thebluealliance.com/team/7447/2023">
+                    The Blue Alliance: CHARGED UP
+                </Link>
+                .
+            </p>
+
+            <br />
+
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 opacity-80 text-sky-600 dark:text-sky-400">
+                &apos;22 RAPID REACT
+            </h1>
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 mb-8 ">
+                Yoshio &apos;22.
+            </h1>
+            <p className="text-xl px-6 sm:px-0 mb-8">
+                View our 2022 season on{" "}
+                <Link className="a-link" href="https://www.thebluealliance.com/team/7447/2022">
+                    The Blue Alliance: RAPID REACT
+                </Link>
+                .
+            </p>
+
+            <br />
+
+
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 opacity-80 text-slate-600 dark:text-slate-400">
+                &apos;20 INFINITE RECHARGE
+            </h1>
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 mb-8 ">
+                Yoshio &apos;20.
+            </h1>
+            <p className="text-xl px-6 sm:px-0 mb-8">
+                View our 2020 season on{" "}
+                <Link className="a-link" href="https://www.thebluealliance.com/team/7447/2020">
+                    The Blue Alliance: INFINITE RECHARGE
+                </Link>
+                .
+            </p>
+
+            <br />
+
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 opacity-80 text-sky-600 dark:text-sky-400">
+                &apos;19 DESTINATION: DEEP SPACE
+            </h1>
+            <h1 className="text-3xl sm:text-4xl font-semibold tracking-tighter px-6 sm:px-0 mb-8 ">
+                Yoshio.
+            </h1>
+            <p className="text-xl px-6 sm:px-0 mb-8">
+                View our 2019 season on{" "}
+                <Link className="a-link" href="https://www.thebluealliance.com/team/7447/2019">
+                    The Blue Alliance: DESTINATION: DEEP SPACE
+                </Link>
+                .
+            </p>
+
+            {/* <CardNavigator />
         <div className="flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory scroll-pl sm:scroll-pl-16 h-[600px] pl-2 pr-2 sm:pl-0 sm:pr-0 ui-intro-cards">
           <div className="ui-intro-card">
               <h2 className="text-3xl font-semibold tracking-tighter">Swerve Drive</h2>
@@ -51,8 +142,8 @@ export default function BotIntroCard() {
               <p className="mt-6 text-lg"><Link className="a-link" href="/robot/reefscape">Learn more information and highlights about our new robot for the 2025 season.</Link></p>
           </div> 
       </div>*/}
-     
-          
-    </div>
+
+
+        </div>
     )
 }
