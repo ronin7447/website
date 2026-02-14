a74bf57090c8d8910067c375fd60edfef9fbf41a
Jun Yu <awa@outlook.ie>
2026-02-14 14:58:32 -0800

    feat: add merch order link

---
+++ app/merch26/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function Join25Page() {
+    redirect('https://docs.google.com/forms/d/e/1FAIpQLSd_ukbMF7mj8eGRADFuwVkJVwK1R3q_x2jCkVSoOxdmdW6khA/viewform?usp=dialog')
+}
