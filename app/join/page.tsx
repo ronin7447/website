cd86dd81b37a38dd4525354d6fa60f83cbcc40d6
Jun Yu <awa@outlook.ie>
2026-03-11 19:18:36 -0700

    feat: add link

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation'
 
 export default function Join25Page() {
-    redirect('https://docs.google.com/forms/d/e/1FAIpQLSeDwUSQIg2a97NV1X8sgn7OiFXGrlpfa8ARMgFlttCn5pFObw/viewform')
+    redirect('https://docs.google.com/forms/d/e/1FAIpQLScR8Jg2eoymheiHkNBgr9woUJ89dXIIbB43Q2MxCFcslyB_aA/viewform')
 }
