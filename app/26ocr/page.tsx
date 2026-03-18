bdb8d1f061c491814806dbbac78005caed8a299e
Jun Yu <awa@outlook.ie>
2026-03-18 11:43:53 -0700

    feat: add link

---
+++ app/26ocr/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function Join25Page() {
+    redirect('https://docs.google.com/forms/d/e/1FAIpQLSdHQlO79wgNnt-Mggas7hHl8xm9SykyspAVYIyhkscY1U3Z_A/viewform')
+}
