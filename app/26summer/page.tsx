209f919a9e2838f22ac5ce008ebac2b36dfd84ee
Jun Yu <awa@outlook.ie>
2026-05-20 12:14:51 -0700

    feat: update

---
+++ app/26summer/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation'
+
+export default function Join25Page() {
+    redirect('https://docs.google.com/forms/d/1Gg2_57JvcSnMVuMJUVDpC8i-08o6sbNrZ2HtDxJ7eFQ/viewform')
+}
